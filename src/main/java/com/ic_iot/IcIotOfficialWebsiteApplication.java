b6c3639f68ded967099729679000eb92ef2cf2b4
xiuge <yanxiguoa@126.com>
2017-03-20 10:22:14 +0800

    第一次提交

---
+++ src/main/java/com/ic_iot/IcIotOfficialWebsiteApplication.java
@@ -0,0 +1,12 @@
+package com.ic_iot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcIotOfficialWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcIotOfficialWebsiteApplication.class, args);
+	}
+}
